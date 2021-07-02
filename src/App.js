4432f4ed5717bbfcb5bbf718621b5f7cfa24fc69
NakshatraCodes <nakshatra163@gmail.com>
2021-07-02 18:26:07 +0530

    tweaks

--- src/App.js
+++ src/App.js
@@ -5,38 +5,42 @@ function App() {
   return (
     <div className="container">
       <div className="heading-container">
-        <h3>Question 1/14</h3>
         <h3>
-          Score : <span>4</span>
+          Question <span>1/12</span>
+        </h3>
+        <h3>
+          Score <span>04</span>
         </h3>
       </div>
       <div className="question">
-        <h1 className="header">Welcome To React-Bootstrap</h1>
+        <h2 className="header">
+          What was the first Android version specifically optimized for tablets?
+        </h2>
       </div>
       <div className="option-container">
         <div className="option">
           <div>
             <span>A</span>
           </div>
-          <h3>Sapien Automate</h3>
+          <h3>HoneyComb</h3>
         </div>
         <div className="option">
           <div>
             <span>B</span>
           </div>
-          <h3>Sapien Automate</h3>
+          <h3>Eclair</h3>
         </div>
         <div className="option">
           <div>
             <span>C</span>
           </div>
-          <h3>Sapien Automate</h3>
+          <h3>Froyo</h3>
         </div>
         <div className="option">
           <div>
             <span>D</span>
           </div>
-          <h3>Sapien Automate</h3>
+          <h3>Marshmellow</h3>
         </div>
       </div>
       <div className="btn-container">
